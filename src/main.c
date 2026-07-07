862bffcd4d31bde09f7181047cc04d8313286fb8
EternallyDebuggingKnight <j.paul.vodeme@proton.me>
2026-07-07 15:06:03 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// determine la commande a executer
+typedef enum {
+  EXIT,
+  NOT_FOUND,
+} Keywords;
+
+Keywords get_command(char *name);
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -26,14 +34,11 @@ int main(int argc, char *argv[]) {
          raw_input[i] != '\0' && k < BUFSIZ && j < (FILENAME_MAX - 1); i++) {
 
       if (!isspace((unsigned char)raw_input[i])) {
-
         token[j] = raw_input[i];
-
         j++;
       } else {
 
         if (token[0] != '\0') {
-
           token[j] = '\0';
           command[k] = strdup(token);
           k++;
@@ -43,10 +48,27 @@ int main(int argc, char *argv[]) {
       };
     };
 
-    if (command[0] != NULL && command[0][0] != '\0')
-      printf("%s: command not found\n", command[0]);
+    if (command[0] != NULL && command[0][0] != '\0') {
+
+      switch (get_command(command[0])) {
+      case EXIT:
+        return 0;
+      case NOT_FOUND:
+        printf("%s: command not found\n", command[0]);
+      }
+    };
 
     printf("$ ");
   };
   return 0;
 }
+
+/*
+ * @brief Compare le nom en entree a une liste de command
+ * @return le numero de la command a execute
+ */
+Keywords get_command(char *name) {
+  if (strcmp(name, "exit") == 0)
+    return EXIT;
+  return NOT_FOUND;
+};
