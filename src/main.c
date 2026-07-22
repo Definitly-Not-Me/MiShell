c00125d14587b83bf1856a3824247ca8d2f4e7fe
EternallyDebuggingKnight <j.paul.vodeme@proton.me>
2026-07-22 01:23:24 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <ctype.h>
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <unistd.h>
+
+#ifdef _WIN32
+#define PATH_LIST_SEPARATOR ';'
+#else
+#define PATH_LIST_SEPARATOR ':'
+#endif
 
 // determine la commande a executer
 typedef enum {
@@ -19,9 +29,9 @@ typedef struct {
 // --- Les functions (Prototypes) ---
 Keywords get_command(char *name);
 char *echo(int argc, char *argv[]);
-char *type(int argc, char *argv[]);
+int type(int argc, char *argv[]);
 
-int main(int argc, char *argv[]) {
+int main(void) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
@@ -83,6 +93,19 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+void getSub(char *s, char *ss, int pos, int l) {
+  int i = 0;
+
+  // Copy substring into ss
+  while (i < l) {
+    ss[i] = s[pos + i];
+    i++;
+  }
+
+  // Null terminate the substring
+  ss[i] = '\0';
+}
+
 /*
  * @brief Compare le nom en entree a une liste de command
  * @return le numero de la command a execute
@@ -110,13 +133,75 @@ char *echo(int argc, char *argv[]) {
 /*
  * @brief donne le type d'une commande
  */
-char *type(int argc, char *argv[]) {
-  if (argc <= 1)
-    return 0;
-  if (get_command(argv[1]) == NOT_FOUND) {
-    printf("%s: not found\n", argv[1]);
+int type(int argc, char *argv[]) {
+  char *PATH = getenv("PATH");
+  struct dirent *dir_descriptor;
+  char entry[FILENAME_MAX];
+  DIR *directory;
+  int i = 1;
+  int j = 0;
+
+  if (argc <= 1) {
+    printf("Insuficient number of arguments\n");
+    return 1;
+  };
+
+  if (get_command(argv[1]) == NOT_FOUND && PATH != NULL) {
+
+    // printf("Command is not buitins. Searching Path.\n");
+
+    while (PATH[i] != '\0') {
+
+      // printf("%d character encountered: %c\n", i, PATH[i]);
+
+      if (PATH[i] == PATH_LIST_SEPARATOR) {
+        getSub(PATH, entry, j, (i - j));
+
+        // printf("New entry: %s\n", entry);
+
+        // Ouvrir le dossier
+        if ((directory = opendir(entry)) == NULL) {
+
+          // printf("Can't open %s\n", entry);
+
+          j = i + 1;
+          i++;
+
+          // printf("i = %d \n j = %d", i, j);
+
+          continue;
+        };
+
+        // Itérer sur chaque fichier du dossier
+        // Et vérifier s'il est égale a argv[1] et
+        // est exécutable
+        while ((dir_descriptor = readdir(directory)) != NULL) {
+          char filename[FILENAME_MAX + 256];
+
+          // Est-ce le fichier rechercher ?
+          if (strcmp((dir_descriptor->d_name), argv[1]) != 0) {
+            continue;
+          };
+
+          snprintf(filename, FILENAME_MAX + 256, "%s/%s", entry,
+                   dir_descriptor->d_name);
+
+          // Checks
+          if (access(filename, F_OK | X_OK) == 0) {
+            printf("%s is %s\n", argv[1], filename);
+
+            return 0;
+          };
+        };
+        j = i + 1;
+      };
+      i++;
+    };
   } else {
     printf("%s is a shell builtin\n", argv[1]);
+    return 0;
   };
+
+  printf("%s: not found\n", argv[1]);
   return 0;
 }
